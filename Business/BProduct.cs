dc911ac4c840962ed5bd3b9c68e28cd1a0e83fbc
Alumno <Alumno@LAB1504-27>
2023-10-17 21:54:43 -0500

    Agregar archivos de proyecto.

---
+++ Business/BProduct.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entity;
+using Data;
+using System.Xml.Linq;
+
+namespace Business
+{
+    public class BProduct
+    {
+        public List<Product> GetByName(string name)
+        {
+            List<Product> result = new List<Product>();
+
+            DProduct data = new DProduct();
+
+            var products = data.Get();
+            
+            foreach (var item in products) 
+            {
+                if (item.name.ToLower() == name.ToLower())
+                {
+                    result.Add(item);
+                }
+            }
+
+            return result;
+        }
+
+        public void Insert(Product product)
+        {
+            DProduct data = new DProduct();
+            data.Insert(product);
+        }
+
+        public void Update(Product product)
+        {
+            DProduct data = new DProduct();
+            data.Update(product);
+        }
+
+        public void Delete(int product_id)
+        {
+            DProduct data = new DProduct();
+            data.Delete(product_id);
+        }
+    }
+}
---
+++ Data/DProduct.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entity;
+
+namespace Data
+{
+    public class DProduct
+    {
+        private readonly string connectionString = "Data Source=LAB1504-27\\SQLEXPRESS;Initial Catalog=Lab5;User ID=hilari;Password=hilari123";
+
+        public List<Product> Get()
+        {
+            List<Product> products = new List<Product>();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                using (SqlCommand command = new SqlCommand("listarProductos", connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Product product = new Product
+                            {
+                                name = reader["Name"].ToString(),
+                                price = (decimal)reader["Price"],
+                                stock = (int)reader["Stock"],
+                                active = (bool)reader["Active"]
+                            };
+                            products.Add(product);
+                        }
+                    }
+                }
+            }
+
+            return products;
+        }
+
+        public void Insert(Product product)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                using (SqlCommand command = new SqlCommand("InsertProduct", connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@name", product.name);
+                    command.Parameters.AddWithValue("@price", product.price);
+                    command.Parameters.AddWithValue("@stock", product.stock);
+                    command.Parameters.AddWithValue("@active", product.active);
+
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public void Update(Product product)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                using (SqlCommand command = new SqlCommand("UpdateProduct", connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@name", product.name);
+                    command.Parameters.AddWithValue("@price", product.price);
+                    command.Parameters.AddWithValue("@stock", product.stock);
+                    command.Parameters.AddWithValue("@active", product.active);
+
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public void Delete(int product_id)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                using (SqlCommand command = new SqlCommand("DeleteProduct", connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@product_id", product_id);
+
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
