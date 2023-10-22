ecdb74d5bc3aab4ff88a13268a18a799e4cd5705
HAms25 <85905195+HAms25@users.noreply.github.com>
2023-10-21 21:43:29 -0500

    Update MainWindow.xaml.cs
    Second commit

--- Labo7/MainWindow.xaml.cs
+++ Labo7/MainWindow.xaml.cs
@@ -27,13 +27,71 @@ namespace Labo7
         public MainWindow()
         {
             InitializeComponent();
-            business = new Business.BProduct();
+            productBusiness = new BProduct();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             string searchName = txtName.Text;
-            List<Product> products = business.GetByName(searchName);
+            List<Product> products = productBusiness.GetByName(searchName);
+
+            dataGrid.ItemsSource = products;
+        }
+
+        private void InsertButton_Click(object sender, RoutedEventArgs e)
+        {
+            string productName = txtProductName.Text;
+            decimal price = Convert.ToDecimal(txtPrice.Text);
+            int stock = Convert.ToInt32(txtStock.Text);
+            bool isActive = chkActive.IsChecked ?? false;
+
+            Product newProduct = new Product
+            {
+                Name = productName,
+                Price = price,
+                Stock = stock,
+                IsActive = isActive
+            };
+
+            productBusiness.Insert(newProduct);
+
+            RefreshDataGrid();
+        }
+
+        private void UpdateButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (dataGrid.SelectedItem is Product selectedProduct)
+            {
+                string updatedName = txtProductName.Text;
+                decimal updatedPrice = Convert.ToDecimal(txtPrice.Text);
+                int updatedStock = Convert.ToInt32(txtStock.Text);
+                bool updatedIsActive = chkActive.IsChecked ?? false;
+
+                selectedProduct.Name = updatedName;
+                selectedProduct.Price = updatedPrice;
+                selectedProduct.Stock = updatedStock;
+                selectedProduct.IsActive = updatedIsActive;
+
+                productBusiness.Update(selectedProduct);
+
+                RefreshDataGrid();
+            }
+        }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (dataGrid.SelectedItem is Product selectedProduct)
+            {
+                productBusiness.Delete(selectedProduct.Id);
+
+                RefreshDataGrid();
+            }
+        }
+
+        private void RefreshDataGrid()
+        {
+            string searchName = txtName.Text;
+            List<Product> products = productBusiness.GetByName(searchName);
 
             dataGrid.ItemsSource = products;
         }
